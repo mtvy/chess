924bd1e1f766d76be223356b0b7443b19279681a
=='mtvy <=='mtvy@gmail.com>
2021-01-13 01:24:32 +0300

    conf_com

---
+++ classes.py
@@ -0,0 +1,9 @@
+### Классы проекта
+from lib import *
+
+class ChessBoard:
+    def __init__(self):
+        self.board = []
+        with open('chess_board.txt') as board_set:  ### Читает из файла макет доски
+            for line in board_set:
+                self.board.append(line.split())
--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-import pygame
+from lib import *
+from classes import *
+
+white,black,red = (255,255,255),(0,0,0),(255,0,0)
+
+def printChessTemplate(chess_board, gameDisplay):
+    for i in range(len(chess_board.board)):
+        print(chess_board.board[i])
+        #for j in i:
+        #    if j == 'w':
+        #        pygame.draw.rect(gameDisplay, white, [x+40*j,y+40*i,20,20])
+        #    else:
+        #        pygame.draw.rect(gameDisplay, red, [x+40*j,y+40*i,20,20])
+    
+
 def main():
     pygame.init()
-
-    white,black,red = (255,255,255),(0,0,0),(255,0,0)
 
     gameDisplay = pygame.display.set_mode((800,600))
 
     pygame.display.set_caption("ChessBoard")
     gameExit = True
+
+    board = ChessBoard() ### Создаёт объект шахматной доски (ChessBoard берет из classes.py)
+    printChessTemplate(board, gameDisplay) ### Выводит в консоль сам макет из файла
+
 
     x = 20
     y = 20
@@ -16,14 +32,16 @@ def main():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     gameExit = False
-            gameDisplay.fill(BLACK)
+            gameDisplay.fill(black)
+            
             for i in range(4):
                 for j in range(4):
-                    pygame.draw.rect(gameDisplay, WHITE, [x+40*j,y+40*i,20,20])
+                    pygame.draw.rect(gameDisplay, white, [x+40*j,y+40*i,20,20])
             pygame.display.update()
             pygame.display.flip()
 
 
     pygame.quit()
+
 if __name__ == '__main__':
     main()

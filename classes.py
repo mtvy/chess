5396b098e84baadb94dc98dd2d1eecb9f0f8ae3b
=='mtvy <=='mtvy@gmail.com>
2021-01-18 20:39:41 +0300

    vvv

--- classes.py
+++ classes.py
@@ -7,3 +7,7 @@ class ChessBoard:
         with open('chess_board.txt') as board_set:  ### Читает из файла макет доски
             for line in board_set:
                 self.board.append(line.split())
+
+class LOL:
+    def __init__(self):
+        pass

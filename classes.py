0297362c2fc28b368e1c3f6e9433d78960b49e31
=='mtvy <=='mtvy@gmail.com>
2021-02-01 00:00:35 +0300

    chess_board_init

--- classes.py
+++ classes.py
@@ -1,13 +1,26 @@
-### Классы проекта
-from lib import *
+class ChessMan:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.tag = '&'
 
-class ChessBoard:
+class Board:
     def __init__(self):
         self.board = []
         with open('chess_board.txt') as board_set:  ### Читает из файла макет доски
             for line in board_set:
                 self.board.append(line.split())
 
-class LOL:
-    def __init__(self):
-        pass
+
+    def print_b(self):
+        for i in self.board:
+            print(*i)
+
+    def enter_obj(self, chess_obj):
+        if type(chess_obj) == ChessMan:
+            print(self.board[chess_obj.x], '\n')
+            print(self.board[chess_obj.x][chess_obj.y], '\n')
+            self.board[chess_obj.x][chess_obj.y] = '&'
+
+a=Board()
+a.print_b()
--- main.py
+++ main.py
@@ -1,43 +1,39 @@
 import pygame
-from PythonApplication1 import *
+from classes import *
+from const import *
 
-pygame.init()
+def main():
+    pygame.init()
 
-white,black,red = (255,255,255),(0,0,0),(255,0,0)
+    gameDisplay = pygame.display.set_mode((360,360))
 
-gameDisplay = pygame.display.set_mode((800,600))
+    pygame.display.set_caption("ChessBoard")
+    gameExit = True
 
-pygame.display.set_caption("ChessBoard")
-gameExit = True
-
-a = Board()
-a.print_b()
-chess_man = ChessMan(3,2)
-a.enter_obj(chess_man)
-a.print_b()
+    a = Board()
+    a.print_b()
 
 
-while  gameExit==True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameExit = False
-    gameDisplay.fill(white)
-    for i in range (4):
-        for j in range(4):
-            pygame.draw.rect(gameDisplay,black,(0+40*i,0+40*j,20,20))
-    for i in range (4):
-        for j in range(4):
-            pygame.draw.rect(gameDisplay,black,(20+40*i,20+40*j,20,20))
-    for i in range(8):
-        for j in range(8):
-            if a.board[i][j] == '&':
-                pygame.draw.rect(gameDisplay,red,(20+40*i,20+40*j,20,20))
+    while  gameExit==True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = False
+        gameDisplay.fill(TESTING_COLOR)
+        for i in range (8):
+            for j in range(8):
+                if a.board[i][j] == 'w':
+                    pygame.draw.rect(gameDisplay, WHITE,(20+i*40,20+j*40,40,40))
+                if a.board[i][j] == 'b':
+                    pygame.draw.rect(gameDisplay,BLACK,(20+i*40,20+j*40,40,40))
 
-    pygame.draw.line(gameDisplay,black,(0,0),(0,160),3)
-    pygame.draw.line(gameDisplay,black,(0,160),(160,160),3)
-    pygame.draw.line(gameDisplay,black,(160,160),(160,0),3)
-    pygame.draw.line(gameDisplay,black,(0,0),(160,0),3)
-    pygame.display.update()
-    pygame.display.flip()
-   
-pygame.quit()
+        pygame.draw.line(gameDisplay,RED,(16,15),(16,340),4)
+        pygame.draw.line(gameDisplay,RED,(16,340),(340,340),4)
+        pygame.draw.line(gameDisplay,RED,(340,340),(340,16),4)
+        pygame.draw.line(gameDisplay,RED,(16,16),(340,16),4)
+        pygame.display.update()
+        pygame.display.flip()
+    
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()

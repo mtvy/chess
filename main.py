8017e66c83a3182fb11c86314fd2a70bd0d26040
=='mtvy <=='mtvy@gmail.com>
2021-01-31 23:30:32 +0300

    main

--- main.py
+++ main.py
@@ -1,49 +1,43 @@
-from lib import *
-from classes import *
+import pygame
+from PythonApplication1 import *
+
+pygame.init()
 
 white,black,red = (255,255,255),(0,0,0),(255,0,0)
 
-def printChessTemplate(chess_board, gameDisplay):
-    for i in range(len(chess_board.board)):
-        print(chess_board.board[i])
-        #for j in i:
-        #    if j == 'w':
-        #        pygame.draw.rect(gameDisplay, white, [x+40*j,y+40*i,20,20])
-        #    else:
-        #        pygame.draw.rect(gameDisplay, red, [x+40*j,y+40*i,20,20])
-    
+gameDisplay = pygame.display.set_mode((800,600))
+
+pygame.display.set_caption("ChessBoard")
+gameExit = True
+
+a = Board()
+a.print_b()
+chess_man = ChessMan(3,2)
+a.enter_obj(chess_man)
+a.print_b()
 
 
+while  gameExit==True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameExit = False
+    gameDisplay.fill(white)
+    for i in range (4):
+        for j in range(4):
+            pygame.draw.rect(gameDisplay,black,(0+40*i,0+40*j,20,20))
+    for i in range (4):
+        for j in range(4):
+            pygame.draw.rect(gameDisplay,black,(20+40*i,20+40*j,20,20))
+    for i in range(8):
+        for j in range(8):
+            if a.board[i][j] == '&':
+                pygame.draw.rect(gameDisplay,red,(20+40*i,20+40*j,20,20))
 
-def main():
-    pygame.init()
-
-    gameDisplay = pygame.display.set_mode((800,600))
-
-    pygame.display.set_caption("ChessBoard")
-    gameExit = True
-
-    board = ChessBoard() ### Создаёт объект шахматной доски (ChessBoard берет из classes.py)
-    printChessTemplate(board, gameDisplay) ### Выводит в консоль сам макет из файла
-
-
-    x = 20
-    y = 20
-
-    while  gameExit==True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    gameExit = False
-            gameDisplay.fill(black)
-            
-            for i in range(4):
-                for j in range(4):
-                    pygame.draw.rect(gameDisplay, white, [x+40*j,y+40*i,20,20])
-            pygame.display.update()
-            pygame.display.flip()
-
-
-    pygame.quit()
-
-if __name__ == '__main__':
-    main()
+    pygame.draw.line(gameDisplay,black,(0,0),(0,160),3)
+    pygame.draw.line(gameDisplay,black,(0,160),(160,160),3)
+    pygame.draw.line(gameDisplay,black,(160,160),(160,0),3)
+    pygame.draw.line(gameDisplay,black,(0,0),(160,0),3)
+    pygame.display.update()
+    pygame.display.flip()
+   
+pygame.quit()
